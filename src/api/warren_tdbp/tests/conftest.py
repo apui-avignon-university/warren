7d4c1186cddcf59f8704f9f1754213ad9b499d83
Quitterie Lucas <quitterielucas@outlook.fr>
2024-02-20 14:43:47 +0100

    🐛(tests) add missing tests database creation stack
    Duplicated from Warren, the test stack contains the step of a db creation for
    cache features.

---
+++ src/api/warren_tdbp/tests/conftest.py
@@ -0,0 +1,19 @@
+"""Module py.test fixtures."""
+
+# pylint: disable=unused-import
+# ruff: noqa: F401
+
+import pytest
+from warren.tests.fixtures.app import http_client
+from warren.tests.fixtures.asynchronous import anyio_backend
+from warren.tests.fixtures.db import (
+    db_engine,
+    db_session,
+    force_db_test_session,
+)
+
+
+@pytest.fixture
+def non_mocked_hosts() -> list:
+    """pytest-httpx: let requests to warren pass untouched."""
+    return ["localhost"]
