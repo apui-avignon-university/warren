794ec797b7a93ad46763e861ea901c484519181d
Wilfried BARADAT <wilfried.baradat@fun-mooc.fr>
2024-04-22 17:53:03 +0200

    🥅(api) handle errors when converting dataframe to pydantic

--- src/api/plugins/tdbp/warren_tdbp/utils.py
+++ src/api/plugins/tdbp/warren_tdbp/utils.py
@@ -1,8 +1,21 @@
 """Utils for TdbP."""
 
+import logging
+
+from pydantic import ValidationError
+
+logger = logging.getLogger(__name__)
+
 
 def dataframe_to_pydantic(model_class, dataframe):
     """Convert a dataframe to a list of Pydantic model instances."""
     columns = model_class.__annotations__.keys()
 
-    return [model_class(**row[columns]) for _, row in dataframe.iterrows()]
+    model = []
+    for _, row in dataframe.iterrows():
+        try:
+            model.append(model_class(**row[columns]))
+        except ValidationError as e:
+            logger.warning("Could not convert dataframe to pydantic: %s", e)
+            continue
+    return model
